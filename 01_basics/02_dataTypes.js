4d72fcecad666aa9cf209c1bf44a219cef90a338
Bikram Barman <barmanbikram918@gmail.com>
2024-06-15 13:08:27 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer veersion
+
+// alert ( 3 + 3) // are using nodejs, not browser
+
+ console.log( 3 + 3);
+
+ console.log("bikram");
+
+
+
+
+ let name = "Bikram"
+ let age = 18
+ let isLoggedIn = false
+
+ // number => 2 to the power of 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standlone value  // null is an object interview question me pu6a jata hai
+ // undefined => 
+ // symbol => uniqueness ke liye mostly use hota hai
+
+
+ // object 
+
+ console.log(typeof undefined);  // output is an undefined
+ console.log(typeof null);  //output is an object
+ 
