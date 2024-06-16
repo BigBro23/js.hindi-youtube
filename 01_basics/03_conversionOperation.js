095c36ec69024f5bf3c17c4fd04889841b75afdb
Bikram Barman <barmanbikram918@gmail.com>
2024-06-16 08:28:42 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33achf"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+
+
+
+// console.log(typeof valueInNumber);
+
+
+// console.log(valueInNumber);
+
+// when we use "33" its easily converted into 33
+// but when we use "33akhd" its not converted into 33akhd(NaN)
+// when we use true(boolean)-its converted into 1 
+// but when we use false it converted into = 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Bikram" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
